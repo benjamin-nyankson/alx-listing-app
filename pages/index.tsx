71c812429fa8176b88a6a205cadc0ad40909fb76
Benjamin Nyankson <nyanksonbenjamin4@gmail.com>
2025-10-31 22:34:47 +0000

    downgrade tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,9 @@ export default function Home() {
       className={`${geistSans.className} ${geistMono.className} flex items-center justify-center min-h-screen `}
     >
       <main className="">
-        <h1 className="">
-          Welcome
+        {/* TODO: remove this and the associated styles from tailwind.config */}
+        <h1 className=" text-5xl font-bold text-primary font-mono overflow-hidden border-r-2 border-black whitespace-nowrap w-[10ch] animate-typing">
+          Welcomee
         </h1>
       </main>
     </div>
